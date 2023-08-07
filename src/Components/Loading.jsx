566c1b84c7ba37205ea9ebf584310f637363c24a
Abbash Ali <abbasali6021@gmail.com>
2023-08-07 14:50:25 +0530

    fix: change assets folder

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -5,8 +5,7 @@ export default function Loading() {
   return (
     <>
       <div className="w-screen h-screen bg-[#181B1D] flex justify-center items-center">
-        <img className="" src='src/assets/b&w-pokeball.gif' />
-        {/* <img className="26547C" src='src/assets/pk.gif' /> */}
+        <img className="" src='/assets/b&w-pokeball.gif' />
       </div>
     </>
   )
